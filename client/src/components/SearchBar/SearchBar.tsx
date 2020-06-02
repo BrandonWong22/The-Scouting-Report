841582e711f983fdb7c19b9ed78d573ec0da8910
brandonwong22 <brandonwong2608@gmail.com>
2020-06-01 22:39:43 -0400

    implemented auto search feature but it looks like the maxResults prop may be glitched

--- client/src/components/SearchBar/SearchBar.tsx
+++ client/src/components/SearchBar/SearchBar.tsx
@@ -1,23 +1,39 @@
 import React, { Component } from "react";
 import "./SearchBar.scss";
 import { ReactSearchAutocomplete } from "react-search-autocomplete";
+import data from "../../helpers/data.json";
 
 class SearchBar extends Component<{}> {
   state = {
     searchValue: "",
   };
 
+  handleOnSearch = (string, cached) => {
+    console.log(string, cached);
+  };
+
+  handleOnSelect = (item) => {
+    console.log(item);
+  };
+
+  handleOnFocus = () => {
+    console.log("Focused");
+  };
+
   render() {
+    console.log(data);
+
     return (
       <div className="search__searchbar-contents">
         <div className="search__searchbar-div">
           <div className="search__autocomplete-container">
             <form className="search__form">
               <ReactSearchAutocomplete
-                // items={items}
-                // onSearch={handleOnSearch}
-                // onSelect={handleOnSelect}
-                // onFocus={handleOnFocus}
+                items={data.companylist}
+                onSearch={this.handleOnSearch}
+                onSelect={this.handleOnSelect}
+                onFocus={this.handleOnFocus}
+                maxResults={{ value: 7 }}
                 autoFocus
               />
               <button className="search__button" type="submit">
