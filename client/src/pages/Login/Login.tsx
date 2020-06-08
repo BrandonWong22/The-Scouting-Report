f051f5180f0a343e8f1af1fab07937c9981566f9
brandonwong22 <brandonwong2608@gmail.com>
2020-06-07 22:44:35 -0400

    added toggle between login and register mode

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -14,55 +14,140 @@ class Login extends Component<{}, LoginPageState> {
     userPassword: "",
   };
 
+  handleRegisterMode = (event: any) => {
+    event.preventDefault();
+    this.setState({
+      loadLoginPage: false,
+      loadRegisterPage: true,
+    });
+  };
+
+  handleLoginMode = (event: any) => {
+    event.preventDefault();
+    this.setState({
+      loadRegisterPage: false,
+      loadLoginPage: true,
+    });
+  };
+
   render() {
-    return (
-      <div className="login-page">
-        <div className="login-page__container">
-          <div className="login-page__login-register-container">
-            <div className="login-page__button-container login-page__button-container--active">
-              <button className="login-page__mode-button ">Login</button>
+    if (this.state.loadLoginPage) {
+      return (
+        <div className="login-page">
+          <div className="login-page__container">
+            <div className="login-page__login-register-container">
+              <div className="login-page__button-container login-page__button-container--active">
+                <button
+                  className="login-page__mode-button"
+                  onClick={this.handleLoginMode}
+                >
+                  Login
+                </button>
+              </div>
+              <div className="login-page__button-container">
+                <button
+                  className="login-page__mode-button"
+                  onClick={this.handleRegisterMode}
+                >
+                  Register
+                </button>
+              </div>
             </div>
-            <div className="login-page__button-container">
-              <button className="login-page__mode-button">Register</button>
-            </div>
+
+            <form className="login-page__form">
+              <div className="login-page__input-container">
+                <div className="login-page__icon-ctn">
+                  <FontAwesomeIcon
+                    icon={faUser}
+                    className="login-page__fontawesome"
+                  />
+                </div>
+                <input
+                  placeholder="email"
+                  type="text"
+                  className="login-page__input"
+                />
+              </div>
+
+              <div className="login-page__input-container">
+                <div className="login-page__icon-ctn">
+                  <FontAwesomeIcon
+                    icon={faLock}
+                    className="login-page__fontawesome"
+                  />
+                </div>
+                <input
+                  placeholder="password"
+                  type="password"
+                  className="login-page__input"
+                />
+              </div>
+
+              <button className="login-page__login-button">LOGIN</button>
+              <p style={{ margin: 10 }}>OR</p>
+              <Facebook />
+            </form>
           </div>
-
-          <form className="login-page__form">
-            <div className="login-page__input-container">
-              <div className="login-page__icon-ctn">
-                <FontAwesomeIcon
-                  icon={faUser}
-                  className="login-page__fontawesome"
-                />
-              </div>
-              <input
-                placeholder="email"
-                type="text"
-                className="login-page__input"
-              />
-            </div>
-
-            <div className="login-page__input-container">
-              <div className="login-page__icon-ctn">
-                <FontAwesomeIcon
-                  icon={faLock}
-                  className="login-page__fontawesome"
-                />
-              </div>
-              <input
-                placeholder="password"
-                type="password"
-                className="login-page__input"
-              />
-            </div>
-
-            <button className="login-page__login-button">LOGIN</button>
-            <p style={{ margin: 10 }}>OR</p>
-            <Facebook />
-          </form>
         </div>
-      </div>
-    );
+      );
+    } else if (this.state.loadRegisterPage) {
+      return (
+        <div className="login-page">
+          <div className="login-page__container">
+            <div className="login-page__login-register-container">
+              <div className="login-page__button-container ">
+                <button
+                  className="login-page__mode-button"
+                  onClick={this.handleLoginMode}
+                >
+                  Login
+                </button>
+              </div>
+              <div className="login-page__button-container login-page__button-container--active">
+                <button
+                  className="login-page__mode-button"
+                  onClick={this.handleRegisterMode}
+                >
+                  Register
+                </button>
+              </div>
+            </div>
+
+            <form className="login-page__form">
+              <div className="login-page__input-container">
+                <div className="login-page__icon-ctn">
+                  <FontAwesomeIcon
+                    icon={faUser}
+                    className="login-page__fontawesome"
+                  />
+                </div>
+                <input
+                  placeholder="email"
+                  type="text"
+                  className="login-page__input"
+                />
+              </div>
+
+              <div className="login-page__input-container">
+                <div className="login-page__icon-ctn">
+                  <FontAwesomeIcon
+                    icon={faLock}
+                    className="login-page__fontawesome"
+                  />
+                </div>
+                <input
+                  placeholder="password"
+                  type="password"
+                  className="login-page__input"
+                />
+              </div>
+
+              <button className="login-page__login-button">LOGIN</button>
+            </form>
+          </div>
+        </div>
+      );
+    }
   }
 }
 
