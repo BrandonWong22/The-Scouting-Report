72882d4dcbfe8ebcf85461b8d99c97999c138f19
brandonwong22 <brandonwong2608@gmail.com>
2020-06-09 17:41:46 -0400

    added basic cron job to backend which gets up to date stock prices every 10 seconds

--- server/server.js
+++ server/server.js
@@ -2,8 +2,12 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const searchCompany = require("./routes/searchCompany");
-const http = require("http").Server(app);
-const io = require("socket.io")(http);
+// const http = require("http").Server(app);
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const axios = require("axios");
+
+let stockPrice = "";
 
 require("dotenv").config();
 
@@ -11,8 +15,19 @@ app.use(express.json());
 app.use(cors());
 app.use("/search", searchCompany);
 
+function getUpToDateStockPrice() {
+  let url =
+    "https://financialmodelingprep.com/api/v3/quote-short/AAPL?apikey=d084cd25905084810ee3429ed54c83d9";
+  axios.get(url).then((response) => {
+    stockPrice = response.data[0].price;
+    console.log(stockPrice);
+  });
+}
+
+setInterval(getUpToDateStockPrice, 10000);
+
 io.on("connection", function (socket) {
   console.log("a user connected");
 });
 
-http.listen(8080, () => console.log("Server started at 8080"));
+server.listen(8080, () => console.log("Server started at 8080"));
