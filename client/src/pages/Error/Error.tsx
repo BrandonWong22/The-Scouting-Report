f5d40183a1adb8ac494ca9c5c2277af75acbf15e
brandonwong22 <brandonwong2608@gmail.com>
2020-07-07 10:47:39 -0400

    styled error page

--- client/src/pages/Error/Error.tsx
+++ client/src/pages/Error/Error.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import "./Error.scss";
 
 const Error: React.FC = () => {
-  return <div>Error</div>;
+  return (
+    <div className="error">
+      <h1>Sorry... This page does not exist!!!</h1>
+    </div>
+  );
 };
 
 export default Error;
