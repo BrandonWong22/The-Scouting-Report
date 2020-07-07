6d83259dc871220f0585d639dbef4e3481237d35
brandonwong22 <brandonwong2608@gmail.com>
2020-07-07 11:32:34 -0400

    added button to redirect users back to home page when they access an invalid page. component state is declared using react hooks

--- client/src/pages/Error/Error.tsx
+++ client/src/pages/Error/Error.tsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Error.scss";
+import { Redirect } from "react-router-dom";
 
 const Error: React.FC = () => {
-  return (
-    <div className="error">
-      <h1>Sorry... This page does not exist!!!</h1>
-    </div>
-  );
+  //declare component state with useState
+  const [redirect, setRedirect] = useState<Boolean>(false);
+
+  if (!redirect) {
+    return (
+      <div className="error">
+        <div className="error__content-block">
+          <h1>Sorry... This page does not exist!!!</h1>
+          <button onClick={() => setRedirect(true)}>Return Home</button>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="error">
+        <Redirect to={"/"} />
+      </div>
+    );
+  }
 };
 
 export default Error;
