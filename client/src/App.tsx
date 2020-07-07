be541cfa2b30ad21bc2cca46bd7c3ad26e045ee4
brandonwong22 <brandonwong2608@gmail.com>
2020-07-07 10:33:43 -0400

    added route to render error component if route does not exist

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Search from "./pages/Search/Search";
 import Results from "./pages/Results/Results";
 import Login from "./pages/Login/Login";
+import Error from "./pages/Error/Error";
 
 const App: React.FC = () => {
   return (
@@ -12,6 +13,7 @@ const App: React.FC = () => {
           <Route path="/" exact component={Login} />
           <Route path="/search" exact component={Search} />
           <Route path="/results/:id" component={Results} />
+          <Route path="*" exact={true} component={Error} />
         </Switch>
       </BrowserRouter>
     </>
